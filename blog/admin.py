e84dc608241fdf73c47f48d3ac224bfe489360e5
labyrt <labyrtcontact@gmail.com>
2026-08-11 15:11:05 -0300

    Register Post model with Django Admin
    Add PostAdmin class to manage Post model in Django Admin.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Configura a apresentação e a busca de posts no Django Admin."""
+
+    list_display = ("title", "author", "published", "created_at")
+    list_filter = ("published", "created_at")
+    search_fields = ("title", "body", "author__username")
